6eaeaaf95f0cdccfa1cfaf92877a0a0d800df8bf
Xiraya <112642451+Xiraya@users.noreply.github.com>
2024-01-19 10:53:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import textwrap
+from IPython.display import display
+from IPython.display import Markdown
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key='AIzaSyAwuVTBm8qQnV4aJL-KbElV2jTvQOZC7aQ')
+model = genai.GenerativeModel('gemini-pro')
+
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history = [])
+st.title('Welcome to Chat assistant')
+
+def role_to_streamlit(role):
+    if role=='model':
+        return 'assistant'
+    else:
+         return role
+
+for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+        
+if prompt := st.chat_input("What can I do for you?"):
+    st.chat_message('user').markdown(prompt)
+    response = st.session_state.chat.send_message(prompt) 
+    
+    with st.chat_message("assistant"):
+	    st.markdown(response.text)
