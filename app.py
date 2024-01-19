2741981c2d4667fc2a41d130ea469d0764501d75
NakulanT <nakult721@gmail.com>
2024-01-19 14:11:36 +0530

    Added file

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from IPython.display import display
 from IPython.display import Markdown
 import streamlit as st
 import google.generativeai as genai
+from PIL import Image
 
 genai.configure(api_key='AIzaSyAwuVTBm8qQnV4aJL-KbElV2jTvQOZC7aQ')
-tmodel = genai.GenerativeModel('gemini-pro')
-imodel = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-pro')
 
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history = [])
@@ -18,24 +18,35 @@ def role_to_streamlit(role):
         return 'assistant'
     else:
          return role
-    
-def get_gemini_response(input,image):
-    if input!="" and image!="":
-       response = imodel.generate_content([input,image])
-    elif image!="" and text=="":
-        response=imodel.generate_content(image)
-    else:
-       response = tmodel.generate_content(input)
-    return response.text
 
 for message in st.session_state.chat.history:
     with st.chat_message(role_to_streamlit(message.role)):
         st.markdown(message.parts[0].text)
-   
         
-if prompt := st.chat_input("What can I do for you?") or image := st.file_uploader("upload an image", type=['jpg','jpeg','png']):
+if prompt := st.chat_input("What can I do for you?"):
     st.chat_message('user').markdown(prompt)
     response = st.session_state.chat.send_message(prompt) 
     
     with st.chat_message("assistant"):
 	    st.markdown(response.text)
+     
+# for the Image    
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+submit=st.button("Tell me about the image")
+     
+def get_gemini_response(image):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    # if input!="":
+    #    response = model.generate_content([input,image])
+    # else:
+    response = model.generate_content(image)
+    return response.text
+
+if submit:
+    response=get_gemini_response(image)
+    st.subheader("The Response is")
+    st.write(response)
